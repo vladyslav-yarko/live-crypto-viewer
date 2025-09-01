27e6eb9f4f6d112ba0885c4702ed106befae4def
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-01 03:16:41 +0300

    feat(backend): add client session decorator util

--- backend/src/utils/client/client.py
+++ backend/src/utils/client/client.py
@@ -14,3 +14,17 @@ class Client(ABC):
     @abstractmethod
     async def close_session(*args, **kwargs) -> None:
         raise NotImplementedError()
+
+
+def client_session(func):
+    @wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        session_was_already_open = self.client.session is not None and not self.client.session.closed
+        if not session_was_already_open:
+            await self.client.open_session()
+        try:
+            return await func(self, *args, **kwargs)
+        finally:
+            if not session_was_already_open:
+                await self.client.close_session()
+    return wrapper
