057320cd414512a1d19b9614fa13b2fd343f57ac
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-01 03:21:27 +0300

    feat(backend): import or client utils

---
+++ backend/src/utils/client/__init__.py
@@ -0,0 +1,4 @@
+from src.utils.client.client import Client, client_session
+from src.utils.client.http import HTTPClient
+from src.utils.client.html import HTMLClient
+from src.utils.client.json import JSONClient
